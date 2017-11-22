d2e4f8ad895c363c1b7a6a15b8e5147dd83343c9
jose sanchez <jsanch81@eafit.edu.co>
2017-11-22 15:29:42 -0500

    avance

--- project4.py
+++ project4.py
@@ -12,16 +12,37 @@ if __name__ == "__main__":
     k = 3
     group = []
     sc = SparkContext(appName="May_Jose") # SparkContext
-    acasi = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/11*.txt")
+    '''
+    Se encarga de traer en un diccionario todos los documentos con sus respectivos textos.
+    '''
+    acasi = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/*.txt")
+
+    '''
+    Sacamos los nombres de los documentos para mostrarlos en el momento que se realizao la
+    agrupacion.
+    '''
     name = acasi.keys().collect()
+
+    '''
+    se realiza la divicion de cada texto de cada documento por un espacio, para obtener las palabras de todos los documentos, y
+    saber su frecuencia en cada documento.
+    '''
     documents_values = acasi.values().map(lambda line: line.split(" "))
 
+    '''
+    En esta parte se esta realizando la comparacion de cada documento, para obtener las distancias
+    Entre el uno y el otro, al final estos metodos me generan una matriz, la cual sera analizada por KMeans
+    para asignar a cada documento en un cluster diferente.
+    '''
     hashingTF = HashingTF()
     tf = hashingTF.transform(documents_values)
-
     idf = IDF().fit(tf)
     tfidf = idf.transform(tf)
 
+    '''
+    En esta area se le pasa la matriz obtenida por los metodos de agrupamiento, los cuales se encargan de asignar a
+    cada documento en su respectivo cluster, dependiendo de la cercania de un documento con el otro.
+    '''
     clusters_model = KMeans.train(tfidf, k, maxIterations=10)
     clusters = clusters_model.predict(tfidf).collect()
     for i in range(k):
@@ -30,15 +51,6 @@ if __name__ == "__main__":
     for i in clusters:
         group[i].append(name[cont][name[cont].rfind('/')+1:])
         cont += 1
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print(name)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("++++++++++++++++++++++++++++++")
-    print(group)
-    print("++++++++++++++++++++++++++++++")
-    print("clusters:")
-    print(clusters)
     s = sc.parallelize(group)
     s.saveAsTextFile("hdfs:///user/mlopez12/prueba2")
     sc.stop()
-    
