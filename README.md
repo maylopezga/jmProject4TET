167264cab5c3e5614197bc54dad02cbe53ea0dec
maylopezga <maye.lopez16@gmail.com>
2017-11-22 11:14:51 -0500

    Change in the project

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # jmProject4TET
 
 ## Autores
-  *Jose David Sánchez Castrillón*       _jsanch81@eafit.edu.co_
+  *Jose David Sánchez Castrillón*              _jsanch81@eafit.edu.co_
 
-  *Mayerli Andrea López Galeano*               _mlopez12@eafit.edu.co_ 
+  *Mayerli Andrea López Galeano*               _mlopez12@eafit.edu.co_
 
 ## Créditos
 
--- project4.py
+++ project4.py
@@ -15,41 +15,40 @@ if __name__ == "__main__":
     '''
     Se encarga de traer en un diccionario todos los documentos con sus respectivos textos.
     '''
-    acasi = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/*.txt")
+    dictDocuments = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/*.txt")
 
     '''
-    Sacamos los nombres de los documentos para mostrarlos en el momento que se realizao la
-    agrupacion.
+    Sacamos los nombres de los documentos para mostrarlos en el momento en que se realiza la agrupacion.
     '''
-    name = acasi.keys().collect()
+    listNameDoc = dictDocuments.keys().collect()
 
     '''
-    se realiza la divicion de cada texto de cada documento por un espacio, para obtener las palabras de todos los documentos, y
-    saber su frecuencia en cada documento.
+    Se realiza la division de cada texto de cada documento por un espacio, para obtener las palabras de
+    todos los documentos, y saber su frecuencia en cada documento.
     '''
-    documents_values = acasi.values().map(lambda line: line.split(" "))
+    list2WordDocuments = dictDocuments.values().map(lambda line: line.split(" "))
 
     '''
-    En esta parte se esta realizando la comparacion de cada documento, para obtener las distancias
-    Entre el uno y el otro, al final estos metodos me generan una matriz, la cual sera analizada por KMeans
+    En esta parte se esta realizando la comparacion de cada documento, para obtener las distancias entre
+    el uno y el otro, al final estos metodos me generan una matriz, la cual sera analizada por KMeans
     para asignar a cada documento en un cluster diferente.
     '''
     hashingTF = HashingTF()
-    tf = hashingTF.transform(documents_values)
+    tf = hashingTF.transform(list2WordDocuments)
     idf = IDF().fit(tf)
     tfidf = idf.transform(tf)
 
     '''
-    En esta area se le pasa la matriz obtenida por los metodos de agrupamiento, los cuales se encargan de asignar a
-    cada documento en su respectivo cluster, dependiendo de la cercania de un documento con el otro.
+    En esta area se le pasa la matriz obtenida por los metodos de agrupamiento, los cuales se encargan
+    de asignar a cada documento en su respectivo cluster, dependiendo de la cercania de un documento con el otro.
     '''
-    clusters_model = KMeans.train(tfidf, k, maxIterations=10)
-    clusters = clusters_model.predict(tfidf).collect()
+    modelClusters = KMeans.train(tfidf, k, maxIterations=10)
+    C = modelClusters.predict(tfidf).collect()
     for i in range(k):
         group.insert(i,[])
         cont = 0
-    for i in clusters:
-        group[i].append(name[cont][name[cont].rfind('/')+1:])
+    for i in C:
+        group[i].append(listNameDoc[cont][listNameDoc[cont].rfind('/')+1:])
         cont += 1
     s = sc.parallelize(group)
     s.saveAsTextFile("hdfs:///user/mlopez12/prueba2")
